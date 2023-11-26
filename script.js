668f1c7fcbc57e3bde9c931070293bd889d398b9
WairimuAbigaelWairimu <maribeabby@gmail.com>
2023-11-26 23:52:20 +0300

    First commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+    .then(() =>{
+      e.preventDefault();
+      form.reset();
+    })
+  });
+});
+
 function sendToWhatsapp(){
     let number = "+254708167366";
 
@@ -19,33 +39,9 @@ function sendToWhatsapp(){
     + "Phone number :" +phone+ "%0a"
     + "Quantity :" +quantity+ "%0a"
     + "Delivery Option :" +deliveryOption+ "%0a"
-    + "Delivery Option :" +currentLocation+ "%0a"
+    + "Current Location :" +currentLocation+ "%0a"
 
     window.open(url, '_blank').focus();
   }
-  
-      
 
-  window.addEventListener("load", function() {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() => {
-        alert("Success!");
-      })
-    });
-  });
-
- function reset(){
-    const formToReset = document.getElementById('my-form');
-    formToReset.addEventListener('submit', (e) => {
-       e.preventDefault();
-       formToReset.reset();
-    });
- }
+ 
